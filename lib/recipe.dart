7a6d3f20775f80589929e395e45cff91d054c340
mwaidesu <machariamwai@gmail.com>
2024-01-29 11:44:09 +0300

    Add Recipes class

---
+++ lib/recipe.dart
@@ -0,0 +1,15 @@
+class Recipe{
+  String label;
+  String imageUrl;
+
+  ///Add servings /ingredients
+  
+  Recipe(
+    this.label,
+    this.imageUrl,
+  );
+
+  //Add List<Recipe>
+}
+
+//Add Ingrediebt() here
