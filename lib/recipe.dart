63d5a9639575d74cac2c3e09759c11136b46a6a7
mwaidesu <machariamwai@gmail.com>
2024-01-29 11:59:20 +0300

    Add Recipe List

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -1,15 +1,41 @@
-class Recipe{
+class Recipe {
   String label;
   String imageUrl;
 
   ///Add servings /ingredients
-  
+
   Recipe(
     this.label,
     this.imageUrl,
   );
 
   //Add List<Recipe>
+  static List<Recipe> samples = [
+    Recipe(
+      "Spaghetti and Meatballs",
+      "assets/spaghettimeatballs.jpg",
+    ),
+        Recipe(
+      "Tomato Soup",
+      "assets/tomatosoup.jpg",
+    ),
+        Recipe(
+      "Grilled Cheese",
+      "assets/grilledcheese.jpg",
+    ),
+        Recipe(
+      "Chocolate Chip Cookies",
+      "assets/cholateChip.jpg",
+    ),
+        Recipe(
+      "Taco Salad",
+      "assets/tacoSalad.jpg",
+    ),
+        Recipe(
+      "Hawaiian Pizza",
+      "assets/hawaiianPizza.jpg",
+    ),
+  ];
 }
 
 //Add Ingrediebt() here
