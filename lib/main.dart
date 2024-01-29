cff223cbd996f8f4b623a588ef7710196092620e
mwaidesu <machariamwai@gmail.com>
2024-01-29 12:30:53 +0300

    Style recipe card and label

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
           //6
           itemBuilder: (BuildContext context, int index) {
             //7
-            //Update Recipe Card
+            //add Gesture Detector
             return buildRecipeCard(Recipe.samples[index]);
           },
         ),
@@ -75,10 +75,22 @@ class _MyHomePageState extends State<MyHomePage> {
   //ToDo: buildRecipeCard() here
   Widget buildRecipeCard(Recipe recipe) {
     return Card(
-      child: Column(
-        children: <Widget>[Image(image: AssetImage(recipe.imageUrl)),
-        Text(recipe.label)],
-
+      elevation: 2.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0)
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[Image(image: AssetImage(recipe.imageUrl)),
+          const SizedBox(height: 14.0,),
+          Text(recipe.label, style: const TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.w700,
+            fontFamily: "Palantino",
+          ),),],
+        
+        ),
       ),
     );
   }
